f64807749cd1cec2ea47dc03612d32603fc9a423
chaitanyavs20 <103110058+chaitanyavs20@users.noreply.github.com>
2023-05-04 12:24:39 +0530

    Update Unstable Array.cpp

--- Unstable Array.cpp
+++ Unstable Array.cpp
@@ -1,34 +1,31 @@
 //Link:-https://www.codechef.com/problems/COUNTSUB?tab=statement
-#include <bits/stdc++.h>
-#define ll long long int
-
+#include <iostream>
+#include<map>
 using namespace std;
 
-ll countPairs(ll n) {
-ll pairs = n * (n - 1) / 2;
-return pairs;
-}
-
 int main() {
-int t;
-cin >> t;
-while (t--) {
-ll n;
-cin >> n;
-unordered_map<ll, ll> frequency;
-    for (ll i = 0; i < n; i++) {
-        ll num;
-        cin >> num;
-        frequency[num]++;
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    long long n,a;
+	    cin>>n;
+	    
+	     map<long long, long long> m;
+    for(long long i=0;i<n;i++){
+        cin>>a;
+        m[a]++;
     }
-
-    ll totalPairs = countPairs(n);
-    for (auto& [num, freq] : frequency) {
-        ll pairsWithNum = countPairs(freq);
-        totalPairs -= pairsWithNum;
-    }
-
-    cout << totalPairs << endl;
+	    
+	    long long count=0;
+	  
+	    long long total = (n*(n-1))/2;
+	    for(auto it = m.begin(); it != m.end(); ++it){
+    long long b = it->second;
+    count += (b * (b - 1)) / 2;
 }
-return 0;
+
+	    cout<<total-count<<endl;
+	}
+	return 0;
 }
